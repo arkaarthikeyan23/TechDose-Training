6994e0472bbc73aa66b1c3a0bff21c6c669e17cf
arkaarthikeyan23 <89853712+arkaarthikeyan23@users.noreply.github.com>
2022-11-01 12:14:11 +0530

    Recursion Assignment updated

---
+++ Chapters/Recursion/GenerateAllSubsets2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class GenerateAllSubsets2 {
+    static List<List<Integer>> allSubSets = new ArrayList<>();
+
+    public static void generateSubsets(int[] arr, int pos, List<Integer> curr){
+        if(pos == arr.length){
+            allSubSets.add(new ArrayList<>(curr));
+            return;
+        }
+        generateSubsets(arr, pos+1, curr);//Include
+        curr.add(arr[pos]);
+        generateSubsets(arr, pos+1, curr);//Exclude
+        curr.remove(curr.size()-1);//Save the state to it's previous state
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        generateSubsets(arr,0,new ArrayList<>());
+        System.out.println(allSubSets);
+    }
+}
---
+++ Chapters/Recursion/SumOfNnos.java
@@ -0,0 +1,14 @@
+public class SumOfNnos {
+
+    public static int sumOfNnos(int n){
+        if(n==1){
+            return 1;
+        }
+        return n + sumOfNnos(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(sumOfNnos(n));
+    }
+}
